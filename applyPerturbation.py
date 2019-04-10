b4b7b426f285c9a53320ac9e14661c5bd5446834
Naman Ahuja <naman@live.in>
2019-04-09 23:36:18 -0400

    new scripts

--- applyPerturbation.py
+++ applyPerturbation.py
@@ -8,14 +8,14 @@ import Typo
 
 def find_most_toxic_words(my_dict):
     sorted_dict = sorted(my_dict.items(), key=operator.itemgetter(1), reverse=True)
-    return [sorted_dict[0][0]]
+    return [sorted_dict[0][0], sorted_dict[1][0], sorted_dict[2][0]]
 
 
 def insertLeetSpeak():
     count = 0
     i = 1
     with open('data/mondal_json_toxicity.csv') as input_file:
-        with open('perturbed_data/mondal_json_6c_leetspeak', mode='w') as output_file:
+        with open('perturbed_data/mondal_json_leetspeak_3w', mode='w') as output_file:
             reader = csv.reader(input_file, delimiter=',')
             for row in reader:
                 count += 1
@@ -26,7 +26,7 @@ def insertLeetSpeak():
                 most_toxic_words = find_most_toxic_words(toxicity_dict)
                 perturbed_tweet = tweet
                 for word in most_toxic_words:
-                    perturbed_word = leetSpeak.word2Leet(word, 6)
+                    perturbed_word = leetSpeak.word2Leet(word, len(word))
                 # TODO: Check whether this ig a replaceAll or just a single replace.
                     perturbed_tweet = perturbed_tweet.replace(word, perturbed_word)
                 output_file.write(str(i) + ',0,' + perturbed_tweet)
