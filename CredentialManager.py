12820d6a0a4cd5b6c2433f0790aa46db83355d31
SampannaKahu <sampyash@gmail.com>
2019-04-13 22:55:12 -0400

    Underscore. Zwsp. Bug Fix. Credential manager.

---
+++ CredentialManager.py
@@ -0,0 +1,11 @@
+import getpass
+
+
+def get_my_api_key():
+    username = getpass.getuser()
+    if username == 'sampanna':
+        return 'AIzaSyBG_1zyVQwCKnqFoPyz7IjAKCS0xu_KXG0'
+    elif username == 'naman':
+        return 'AIzaSyBU2AZtVmel0wV_NMhPTFKmChVHxb6_30Q'
+    else:
+        return 'AIzaSyBG_1zyVQwCKnqFoPyz7IjAKCS0xu_KXG0'
